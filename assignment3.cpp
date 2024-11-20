76466bcb7bbfe91572a089b3c2a66256f39777f6
Teun van Brakel <teunvbrakel@gmail.com>
2024-11-20 14:29:05 +0100

    added comments

--- assignment3.cpp
+++ assignment3.cpp
@@ -41,6 +41,8 @@ public:
         return list;
     }
 
+    //Method in which the BFS functionality is performed. 
+    //It uses the start location, end location and current maximum time as an input. And outputs the maximum it found.
     int bfs(int start, int end, int maxTime) {
         priority_queue<pair<vertex, int>, vector<pair<vertex, int>>, greater<pair<vertex, int>>> pq;
         unordered_map<int, int> max_time;
@@ -55,13 +57,13 @@ public:
             auto [curr_vertex, curr_node] = pq.top();
             pq.pop();
             
-            // Explore the neighbors
+            // Explore the neighbors for each edge
             for (const Edge& edge : list[curr_node]) {
                 if (curr_vertex.arrivalTime <= edge.time && curr_node != end) {
                     waitTime = curr_vertex.waitTime + edge.time - curr_vertex.arrivalTime;
                     t.arrivalTime = edge.time + edge.weight;
                     t.waitTime = waitTime;
-                    // Only proceed if we find a shorter path to the neighbor
+                    // Only update the maximum time if the current maximum waittime is larger.
                     if (!max_time[edge.destination] || waitTime > max_time[edge.destination]) {
                         max_time[edge.destination] = waitTime;
                     }if(edge.destination != end){
@@ -73,6 +75,7 @@ public:
         return max_time[end];
     }
 
+    //Method to change the input string to a list of integers.
     vector<int> change_string_to_list_of_int(const string& input){
         vector<int> result;
         stringstream str_strm(input);
@@ -83,6 +86,7 @@ public:
         return result;
     }
 
+    //Method which adds an edge between each location of a busline to create a graph.
     startingPoints add_busLine(const vector<int>& stops, const vector<int>& times, int count, int totalStops, int max){
         startingPoints result;
         bool containsStart = false;
@@ -106,6 +110,7 @@ public:
     unordered_map<int, unordered_map<int, int>> location_time_to_id;
     int next_id = -1;
 
+    //Create a location timestamp to easily have for each location multiple moments in time from which an edge will departure.
     int get_id(int location, int time) {
         if (!location_time_to_id[location].count(time)) {
             location_time_to_id[location][time] = next_id;
@@ -117,6 +122,7 @@ public:
 
 int main()
 {
+    //Initializing the graph.
     Graph graph;
     int numberOfBusLines; cin >> numberOfBusLines;
     int numberOfBusstops; cin >> numberOfBusstops;
@@ -125,6 +131,7 @@ int main()
     unordered_map<int, startingPoints> startBus;
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     
+    //Creating the graph with the input.
     for (int i = 0; i < numberOfBusLines; i++)
     {
         string busStops;
@@ -146,11 +153,13 @@ int main()
     bool exit = false;
     int maxTime = 0;
     auto g = graph.get_list();
+    //Get the maxTime if you would wait at start.
     for (int i = 0; i < startBus.size(); i++) {
         if(startBus[i].time > maxTime){
             maxTime = startBus[i].time;
         }
     }
+    //Performing BFS on the graph. And printing the result.
     if(exit == false){
         int result = graph.bfs(0, numberOfBusstops-1, maxTime);
 
